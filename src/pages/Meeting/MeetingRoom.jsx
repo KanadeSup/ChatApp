40ea44e585054ac6c48f2daab863c3ee85573046
KanadeSup <kanadesup@gmail.com>
2023-12-27 09:35:05 +0700

    add meeting

--- src/pages/Meeting/MeetingRoom.jsx
+++ src/pages/Meeting/MeetingRoom.jsx
@@ -1,42 +1,61 @@
 import { vi } from "date-fns/locale";
 import { Camera, CameraOff, LogOut, Mic, MicOff } from "lucide-react";
 import { useEffect, useRef, useState } from "react";
+import { UserVideo } from "./UserVideo";
 
-function MeetingRoom({ session, subcribers, publisher }) {
-   const videoRef1 = useRef();
-   const videoRef2 = useRef();
+function MeetingRoom({ session, subcribers, publisher, leaveSession }) {
    const [isMicEnable, setIsMicEnable] = useState(false);
    const [isCamEnable, setIsCamEnable] = useState(false);
-   useEffect(()=> {
-      if(!publisher &&!!videoRef1) return
-      publisher.addVideoElement(videoRef1.current)
-      console.log("subcriber", subcribers)
-      if(subcribers.length === 0) return 
-      subcribers[0].addVideoElement(videoRef2.current)
-   }, [subcribers, publisher])
-
    return (
       <div className="w-full h-screen flex flex-col">
          {/* Camera section */}
-         <div className="flex justify-center items-stretch h-full gap-5 p-5 flex-wrap">
-            <video style={{}} autoPlay={true} ref={videoRef1} className="border border-black rounded flex-grow" />
-            <video autoPlay={true} ref={videoRef2} className="border border-black rounded flex-grow" />
+         <div 
+            className={`grid grid-flow-row gap-4 h-full items-center justify-items-center p-5 flex-grow-0 min-h-0 min-w-0
+               ${
+                  subcribers.length === 0 ? "grid-cols-1" : 
+                  subcribers.length > 0 && subcribers.length <= 3 ? "grid-cols-2" :
+                  subcribers.length > 3 && subcribers.length <= 8 ? "grid-cols-3" :
+                  subcribers.length > 8 && subcribers.length <= 15 ? "grid-cols-4" :
+                  subcribers.length > 15 ? "grid-cols-5" : ""
+               }`
+            }
+         >
+            <UserVideo streamManager={publisher} className="" />
+
+            {
+               subcribers.length !== 0 ? 
+                  subcribers.map(subcriber => (
+                     <UserVideo streamManager={subcriber} className="" />
+                  )) : null
+            }
          </div>
 
          {/* toolbar */}
          <div className="h-14 flex justify-center gap-5 items-center rounded mx-3 mb-3">
-            <button className="border border-gray-300 rounded-lg p-2 bg-red-500" onClick={(e) => {}}>
+
+            {/* Logout */}
+            <button className="border border-gray-300 rounded-lg p-2 bg-red-500" onClick={(e) => {leaveSession()}}>
                <LogOut className="stroke-white stroke-[3]" />
             </button>
+
+            {/* Mic */}
             <button
                className={`border border-gray-300 rounded-lg p-2 ${isMicEnable ? "bg-green-600" : "bg-gray-400"}`}
-               onClick={(e) => setIsMicEnable(!isMicEnable)}
+               onClick={(e) => {
+                  setIsMicEnable(!isMicEnable)
+                  publisher.publishAudio(!isMicEnable)
+               }}
             >
                {isMicEnable ? <Mic className="stroke-white stroke-[2]" /> : <MicOff className="stroke-black stroke-[2]"/>}
             </button>
+
+            {/* Camera */}
             <button
                className={`border border-gray-300 rounded-lg p-2 ${isCamEnable ? "bg-green-600" : "bg-gray-400"}`}
-               onClick={(e) => setIsCamEnable(!isCamEnable)}
+               onClick={(e) => {
+                  setIsCamEnable(!isCamEnable)
+                  publisher.publishVideo(!isCamEnable)
+               }}
             >
                {isCamEnable ? <Camera className="stroke-white stroke-[2]" /> : <CameraOff className="stroke-black stroke-[2]"/>}
             </button>
